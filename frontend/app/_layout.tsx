5e2fedc46000c6ae560f38e559ebf14843694081
medeirosJose <62633628+medeirosJose@users.noreply.github.com>
2024-10-23 02:15:53 -0300

    new files setup for tabs to work

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,13 +1,12 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+import React from 'react';
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
 import { PaperProvider } from 'react-native-paper';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -16,26 +15,13 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
 
   return (
     <PaperProvider>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-          <Stack.Screen name='+not-found' />
+          <Stack.Screen name='index' options={{ headerShown: false }} />
         </Stack>
       </ThemeProvider>
     </PaperProvider>
